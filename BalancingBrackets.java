022c56b80eb89e09b827a1e3405f180a93218259
Ketan narhe <ketannarhe02@gmail.com>
2025-09-26 11:32:34 +0530

    Add files via upload

---
+++ BalancingBrackets.java
@@ -0,0 +1,44 @@
+package Collection;
+
+public class BalancingBrackets {
+
+	public static void main(String[] args) {
+		
+		String s="{{()}";
+		
+		System.out.println(isBalanaced(s));
+
+	}
+	
+	
+	public static boolean isBalanaced(String s)
+	{
+		if(s.length()!=0 && s.length()%2==0)
+		{
+			Stack<Character>chars =new Stack<Character>();
+			
+			for(int i=0;i<s.length();i++)
+			{
+				char ch=s.charAt(i);
+				
+				if(ch=='{' || ch=='[' ||ch=='(')
+				{
+					chars.push(ch);
+				}
+				else if(!chars.isEmpty() && (chars.peek()=='{' && ch=='}' || chars.peek()=='['&& ch==']' ||chars.peek()=='(' && ch==')'))
+			{
+					 chars.pop();
+			}
+				else
+				{
+					return false;
+				}	
+			}
+			return chars.isEmpty();
+		}
+		return false;
+		
+		
+	}
+
+}
